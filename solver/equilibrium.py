5f14771605bb9b3223b275da59719e20c8909631
Jan Medlock <jan.medlock@oregonstate.edu>
2021-11-23 20:36:14 -0800

    Keep y.sum() constant when finding equilibria and limit cycles.

--- solver/equilibrium.py
+++ solver/equilibrium.py
@@ -3,17 +3,21 @@
 import scipy.optimize
 
 from . import solution
+from . import utility
 
 
-def _objective(y, func, t):
-    return func(t, y)
+def _objective(x, func, t, transform):
+    y = transform.inverse(x)
+    dy = func(t, y)
+    return transform(dy)
 
 
 def find(func, t, y_guess, states=None):
-    '''Find an equilibrium.'''
+    '''Find an equilibrium while keeping `y_guess.sum()` constant.'''
+    transform = utility.TransformConstantSum(y_guess)
     result = scipy.optimize.root(_objective,
-                                 y_guess,
-                                 args=(func, t))
+                                 transform(y_guess),
+                                 args=(func, t, transform))
     assert result.success, f'{result}'
-    y = result.x
+    y = transform.inverse(result.x)
     return solution.Solution(y, states=states)
--- solver/limit_cycle.py
+++ solver/limit_cycle.py
@@ -7,15 +7,15 @@ from . import solver
 from . import utility
 
 
-def _poincaré_map(y_0, solver_, t, y):
+def _objective(x, solver_, t, y, transform):
     '''Find the state after one period.'''
-    # Solve starting from `y_0`.
+    y_0 = transform.inverse(x)
     solver_(t, y_0, y=y, _solution=False)
-    return y[-1]
+    return transform(y[-1])
 
 
 def find(func, t_0, period, t_step, y_0_guess, **kwds):
-    '''Find a limit cycle.'''
+    '''Find a limit cycle while keeping `y_0.sum()` constant.'''
     # Instead of calling `solver.solve()`, reuse many of the
     # `solver.Solver()` parts for speed.
     solver_ = solver.Solver.create(func, **kwds)
@@ -24,9 +24,12 @@ def find(func, t_0, period, t_step, y_0_guess, **kwds):
     # Storage for intermediate y values.
     y = numpy.empty((len(t), *numpy.shape(y_0_guess)))
     # Find the `y_0` value that is a fixed point of the Poincaré map,
-    # i.e. that gives `y(t_0 + period) = y_0`.
-    y_0 = scipy.optimize.fixed_point(_poincaré_map,
-                                     y_0_guess,
-                                     args=(solver_, t, y))
+    # i.e. that gives `y(t_0 + period) = y_0`, while keeping
+    # `y_0.sum()` constant.
+    transform = utility.TransformConstantSum(y_0_guess)
+    x = scipy.optimize.fixed_point(_objective,
+                                   transform(y_0_guess),
+                                   args=(solver_, t, y, transform))
+    y_0 = transform.inverse(x)
     # Return the solution at all of the intermediate t values.
     return solver_(t, y_0, y=y)
