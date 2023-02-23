094ef1ea2215cbd37203fae41999b6b4d3b61bb6
Jan Medlock <jan.medlock@oregonstate.edu>
2023-02-23 16:40:04 -0600

    Update scripts.

--- age_structured.py
+++ age_structured.py
@@ -2,8 +2,6 @@
 '''Based on our FMDV work, this is an age-structured model with
 periodic birth rate.'''
 
-import time
-
 import matplotlib.pyplot
 
 import models
@@ -12,18 +10,19 @@ import models.age_structured
 
 if __name__ == '__main__':
     (t_start, t_end) = (0, 1)
+    plot_states = ['susceptible', 'infectious', 'recovered']
 
-    # model_constant = models.age_structured.Model(birth_variation=0)
-    # solution_constant = model_constant.solve((t_start, t_end))
-    # totals_constant = solution_constant.age.aggregate()
-    # ax_solution = totals.plotting.solution()
+    model_constant = models.age_structured.Model(birth_variation=0)
+    solution_constant = model_constant.solve((t_start, t_end))
+    ax_solution = solution_constant.age.aggregate() \
+                                   .plotting.solution()
+    equilibrium = model_constant.find_equilibrium(solution_constant.loc[t_end])
+    ax_state = equilibrium.age.aggregate() \
+                          .plotting.state(states=plot_states)
 
     model = models.age_structured.Model()
-    t0 = time.time()
     solution = model.solve((t_start, t_end))
-    print('Runtime {} sec.'.format(time.time() - t0))
-    totals = solution.age.aggregate()
-    # totals.plotting.solution(ax=ax_solution, legend=False)
-    totals.plotting.solution()
+    solution.age.aggregate() \
+            .plotting.solution(ax=ax_solution, legend=False)
 
     matplotlib.pyplot.show()
--- time_since_entry_structured.py
+++ time_since_entry_structured.py
@@ -10,16 +10,20 @@ import models.time_since_entry_structured
 
 if __name__ == '__main__':
     (t_start, t_end) = (0, 10)
+    plot_states = ['susceptible', 'infectious', 'recovered']
 
     model_constant = models.time_since_entry_structured.Model(
         birth_variation=0)
     solution_constant = model_constant.solve((t_start, t_end))
-    totals_constant = solution_constant.time_since_entry.aggregate()
-    ax_solution = totals_constant.plotting.solution()
+    ax_solution = solution_constant.time_since_entry.aggregate() \
+                                   .plotting.solution()
+    equilibrium = model_constant.find_equilibrium(solution_constant.loc[t_end])
+    ax_state = equilibrium.time_since_entry.aggregate() \
+                          .plotting.state(states=plot_states)
 
     model = models.time_since_entry_structured.Model()
     solution = model.solve((t_start, t_end))
-    totals = solution.time_since_entry.aggregate()
-    totals.plotting.solution(ax=ax_solution, legend=False)
+    solution.time_since_entry.aggregate() \
+            .plotting.solution(ax=ax_solution, legend=False)
 
     matplotlib.pyplot.show()
