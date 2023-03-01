e58a6a85f4e3a8c0cbad3621f9fde523d4046d41
Jan Medlock <jan.medlock@oregonstate.edu>
2023-03-01 13:21:37 -0600

    Added option to solve to get close to an equilibrium before using the root solver.

--- models/_model/equilibrium.py
+++ models/_model/equilibrium.py
@@ -5,6 +5,13 @@ import numpy
 from .._utility import linalg, numerical, optimize
 
 
+def _solve(model, y_0, t, t_solve):
+    '''Solve from `t` to `t + t_solve` to get close to an
+    equilibrium.'''
+    t_span = (t, t + t_solve)
+    return model._solver.solution_at_t_end(t_span, y_0)
+
+
 def _objective(y_cur, solver, t, weights):
     '''Helper for `find`.'''
     y_new = solver.step(t, y_cur)
@@ -12,11 +19,13 @@ def _objective(y_cur, solver, t, weights):
     return diff
 
 
-def find(model, y_guess, t=0, weights=1, **root_kwds):
+def find(model, y_guess, t=0, t_solve=0, weights=1, **root_kwds):
     '''Find an equilibrium `y` while keeping
     `weighted_sum(y, weights)` constant.'''
     # Ensure `y_guess` is nonnegative.
     y_guess = numpy.clip(y_guess, 0, None)
+    if t_solve > 0:
+        y_guess = _solve(model, y_guess, t, t_solve)
     result = optimize.root(_objective, y_guess,
                            args=(model._solver, t, weights),
                            sparse=model._solver._sparse,
