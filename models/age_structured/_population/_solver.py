9d375f842663c6f9b9b3236e62f1fc609bbd367e
Jan Medlock <jan.medlock@oregonstate.edu>
2023-03-05 15:36:02 -0600

    Use shift to speed finding dominant eigenvalues.

--- models/age_structured/_population/_solver.py
+++ models/age_structured/_population/_solver.py
@@ -143,44 +143,64 @@ class Solver:
                                    display=display)
         return Phi_new
 
+    def multiplier_from_growth_rate(self, growth_rate):
+        '''Convert a growth rate into a multiplier.'''
+        multiplier = numpy.exp(growth_rate * self.period)
+        return multiplier
+
+    def growth_rate_from_multiplier(self, multiplier):
+        '''Convert a growth rate into a multiplier.'''
+        growth_rate = numpy.log(multiplier) / self.period
+        return growth_rate
+
     # TODO: Consider caching this method.
-    def population_growth_rate(self, birth_scaling, **kwds):
+    def population_growth_rate(self, birth_scaling, _guess=None, **kwds):
         '''Get the population growth rate.'''
         Psi = self.monodromy(birth_scaling, **kwds)
         # Get the dominant Floquet multiplier.
+        sigma = (self.multiplier_from_growth_rate(_guess)
+                 if _guess is not None
+                 else None)
         rho_dom = _utility.linalg.get_dominant_eigen(Psi, which='LM',
+                                                     sigma=sigma,
                                                      return_eigenvector=False)
         # Convert the dominant Floquet multiplier to
         # the dominant Floquet exponent.
-        mu_dom = numpy.log(rho_dom) / self.period
+        mu_dom = self.growth_rate_from_multiplier(rho_dom)
+        print(f'{birth_scaling=}: {mu_dom=}')
         return mu_dom
 
     # TODO: Cache this method.
     def birth_scaling_for_zero_population_growth(self, **kwds):
         '''Find the birth scaling that gives zero population growth rate.'''
-        # Set `kwds`, if any.
-        fcn = functools.partial(self.population_growth_rate, **kwds)
+        # Set keyword arguments. In particular, we are looking for
+        # growth rate of 0.
+        growth_rate = functools.partial(self.population_growth_rate,
+                                        _guess=0, **kwds)
         # `.population_growth_rate()` is increasing in
         # `birth_scaling`. Find a starting bracket `(lower, upper)` with
         # `.population_growth_rate(upper) > 0` and
         # `.population_growth_rate(lower) < 0`.
         SCALE = 2
         upper = 1  # Starting guess.
-        while fcn(upper) < 0:
+        while growth_rate(upper) < 0:
             upper *= SCALE
         lower = upper / SCALE  # Starting guess.
-        while fcn(lower) > 0:
+        while growth_rate(lower) > 0:
             (lower, upper) = (lower / SCALE, lower)
-        scaling = scipy.optimize.brentq(fcn, lower, upper)
+        scaling = scipy.optimize.brentq(growth_rate, lower, upper)
         return scaling
 
     # TODO: Cache this method.
     def stable_age_density(self, **kwds):
         '''Get the stable age density.'''
         Psi = self.monodromy(**kwds)
-        (_, v_dom) = _utility.linalg.get_dominant_eigen(
-            Psi, which='LM', return_eigenvector=True
+        # The growth rate is 0.
+        sigma = self.multiplier_from_growth_rate(0)
+        (rho_dom, v_dom) = _utility.linalg.get_dominant_eigen(
+            Psi, which='LM', sigma=sigma, return_eigenvector=True
         )
+        assert numpy.isclose(self.growth_rate_from_multiplier(rho_dom), 0)
         # Normalize `v_dom` in place so that its integral over a is 1.
         v_dom /= _integral.over_a(v_dom, self.a_step)
         return (self.a, v_dom)
