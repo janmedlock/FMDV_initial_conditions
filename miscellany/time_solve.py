ad3d94e8d692dc4c29cb6f967f0472bf0ab11365
Jan Medlock <jan.medlock@oregonstate.edu>
2023-04-17 18:18:06 -0600

    Add script to time solving models.

---
+++ miscellany/time_solve.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+import time
+
+import matplotlib.pyplot
+import numpy
+import pandas
+import scipy.stats
+
+from context import models
+
+
+def time_solve(Model, t_step, t_span,
+               display=True, check_nonnegative=False,
+               **kwds):
+    model = Model(t_step=t_step, **kwds)
+    y_start = model.build_initial_conditions()
+    t0 = time.perf_counter()
+    y_end = model.solution_at_t_end(t_span,
+                                    y_start=y_start,
+                                    display=display,
+                                    check_nonnegative=check_nonnegative)
+    t = time.perf_counter() - t0
+    return t
+
+
+def time_solves(Model, t_steps, t_span, **kwds):
+    times = [
+        time_solve(Model, t_step, t_span, **kwds)
+        for t_step in t_steps
+    ]
+    index = pandas.Index(t_steps, name='t_step')
+    name = 'run time (sec)'
+    return pandas.Series(times, index=index, name=name)
+
+
+def plot_times(times, t_step_default):
+    ax = times.plot(ylabel=times.name, marker='o', logx=True, logy=True)
+    ax.invert_xaxis()
+    x = numpy.log(times.index)
+    y = numpy.log(times)
+    model = scipy.stats.linregress(x, y)
+    x_ = numpy.linspace(times.index.max(), t_step_default, 101)
+    y_ = numpy.exp(model.slope * numpy.log(x_) + model.intercept)
+    ax.plot(x_, y_, color='black', linestyle='dashed', zorder=1)
+    return ax
+
+
+if __name__ == '__main__':
+    Model = models.combination.Model
+    t_steps = [1e-1, 5e-2, 2e-2, 1e-2]
+    t_span = (0, 1)
+    kwds = dict(transmission_rate=10,
+                _solver_options=dict(_check_matrices=False))
+    times = time_solves(Model, t_steps, t_span, **kwds)
+    ax = plot_times(times, Model._t_step_default)
+    matplotlib.pyplot.show()
+
