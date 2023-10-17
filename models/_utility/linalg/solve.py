f5b8ebcbe02456c99f1ca344b2a622c02bb374e3
Jan Medlock <jan.medlock@oregonstate.edu>
2023-10-17 16:54:07 -0700

    Tweak variable names.

--- models/_utility/linalg/solve.py
+++ models/_utility/linalg/solve.py
@@ -4,15 +4,15 @@ import scipy.linalg
 import scipy.sparse
 
 
-def solve(arr, vec, overwrite_a=False, overwrite_b=False):
+def solve(a, b, overwrite_a=False, overwrite_b=False):
     '''Solve the matrix system A @ x = b.'''
-    if scipy.sparse.issparse(arr):
-        if scipy.sparse.issparse(vec):
-            arr = arr.tocsc()
-            vec = vec.tocsc()
-        result = scipy.sparse.linalg.spsolve(arr, vec)
+    if scipy.sparse.issparse(a):
+        if scipy.sparse.issparse(b):
+            a = a.tocsc()
+            b = b.tocsc()
+        result = scipy.sparse.linalg.spsolve(a, b)
     else:
-        result = scipy.linalg.solve(arr, vec,
+        result = scipy.linalg.solve(a, b,
                                     overwrite_a=overwrite_a,
                                     overwrite_b=overwrite_b)
     return result
