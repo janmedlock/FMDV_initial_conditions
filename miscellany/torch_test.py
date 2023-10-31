ff1859f58696a4c0a0c8e0038ba357984ab7770c
Jan Medlock <jan.medlock@oregonstate.edu>
2023-10-30 19:25:56 -0700

    Cleanup torch_test.py.

--- miscellany/torch_test.py
+++ miscellany/torch_test.py
@@ -1,44 +1,81 @@
 #!/usr/bin/python3
+'''Solve a toy problem with PyTorch.'''
 
+import functools
 import math
 
 import matplotlib.pyplot
 import torch
 
 
-NPOINTS = 2000
-ORDER = 3
-LEARNING_RATE = 1e-3
-NITER = 2000
-
-# Create input and output data
-x = torch.linspace(-math.pi, math.pi, NPOINTS)
-y = torch.sin(x)
+def build_data(npoints):
+    '''Create input and output data.'''
+    input_ = torch.linspace(-1, 1, npoints)
+    target = torch.sin(math.pi * input_)
+    return (input_, target)
 
 
-class Model:
+class Polynomial:
+    '''A univariate polynomial.'''
+
+    # `_polynomial(input_, degree, *, out=None)` basis functions.
+    _polynomial = torch.special.legendre_polynomial_p
+
+    # Use the mean-squared error for the loss function.
+    loss = torch.nn.MSELoss()
+
+    # Use the stochastic gradient decent optimizer.
+    Optimizer = torch.optim.SGD
+
     def __init__(self, order):
-        self.parameters = torch.randn(order + 1,
+        self.order = order
+        # Initialize the parameters to random values and include the
+        # gradient.
+        self.parameters = torch.randn(self.order + 1,
                                       requires_grad=True)
 
-    def __call__(self, x):
-        x_pow_n = x.unsqueeze(-1) \
-                   .pow(torch.arange(len(self.parameters)))
-        y = x_pow_n @ self.parameters
-        return y
+    # Build `_degrees` once for efficiency.
+    @functools.cached_property
+    def _degrees(self):
+        return torch.arange(self.order + 1).unsqueeze(-1)
+
+    def _polynomials(self, input_, *, out=None):
+        '''The (order, len(input_)) tensor
+        [polynomial(input_, 0),
+         polynomial(input_, 1),
+         ...
+         polynomial(input_, order)].'''
+        return self._polynomial(input_, self._degrees, out=out)
+
+    def forward(self, input_, *, out=None):
+        '''The forward model.'''
+        return self.parameters @ self._polynomials(input_, out=out)
+
+    # Simplify calling the forward model.
+    __call__ = forward
+
+    def optimize(self, input_, target, niter, **kws):
+        '''Do `niter` steps of the optimizer.'''
+        optimizer = self.Optimizer([self.parameters], **kws)
+        for _ in range(niter):
+            optimizer.zero_grad()
+            # Compute the gradient with respect to the model parameters.
+            self.loss(self.forward(input_), target).backward()
+            optimizer.step()
 
 
-model = Model(ORDER)
-loss_fn = torch.nn.MSELoss()
-optimizer = torch.optim.SGD([model.parameters],
-                            lr=LEARNING_RATE)
-for i in range(NITER):
-    optimizer.zero_grad()
-    y_pred = model(x)
-    loss = loss_fn(y_pred, y)
-    loss.backward()
-    optimizer.step()
-print(f'{model.parameters=}')
-matplotlib.pyplot.plot(x, y,
-                       x, model(x).detach())
-matplotlib.pyplot.show()
+if __name__ == '__main__':
+    (input_, target) = build_data(npoints=2000)
+
+    model = Polynomial(order=3)
+    model.optimize(input_, target, niter=2000, lr=1e-3)
+    print(f'{model.parameters=}')
+
+    with torch.no_grad():
+        prediction = model(input_)
+    axes = matplotlib.pyplot.gca()
+    axes.plot(input_, target, label='target')
+    axes.plot(input_, prediction, label='prediction')
+    axes.set_xlabel('input_')
+    axes.legend()
+    matplotlib.pyplot.show()
