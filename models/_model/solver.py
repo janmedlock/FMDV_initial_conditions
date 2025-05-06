0d6090cefe5629fb84e53c886854093e13bbc55f
Jan Medlock <jan.medlock@oregonstate.edu>
2025-05-06 15:49:17 -0700

    Add clarifying comment.

--- models/_model/solver.py
+++ models/_model/solver.py
@@ -227,7 +227,10 @@ class Solver(Population, metaclass=abc.ABCMeta):
                                            sparse=self.sparse,
                                            **self._root_kwds)
         elif solver == 'fixed_point':
-            # TODO: Should the first agrument instead be 'new'?
+            # A_hat_B_new = self.A_hat_new
+            #               + self.t_step / 2 * b_mid * self.B
+            # is `self._cn_op('cur', ...)`,
+            # not `self._cn_op('new', ...)`.
             # pylint: disable-next=invalid-name
             A_hat_B_new = self._cn_op('cur',
                                       self.A_hat_new,
